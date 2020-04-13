11eb9a4db606382822c65da557afd79ba98fa01d
Mary Oh <mary.christine.oh@gmail.com>
2020-04-13 16:16:37 -0700

    added pe module 1 and 2

---
+++ Module 1/PE01_Oh/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string output = "Hello World!";
+
+            // Prints value in the output variable
+            Console.WriteLine(output);
+
+            // Pauses console until user responds or presses enter key
+            Console.ReadLine();
+       }
+    }
+}
