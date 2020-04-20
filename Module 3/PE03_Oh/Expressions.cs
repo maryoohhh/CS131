d177a90f894afff885f6c7b87b4fa99b7a80afc0
Mary Oh <mary.christine.oh@gmail.com>
2020-04-20 15:34:40 -0700

    Includes all works required

---
+++ Module 3/PE03_Oh/Expressions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Expressions
+{
+    class Expressions
+    {
+        static void Main(string[] args)
+        {
+            // initializing values
+            int a = 100;
+            int b = 20;
+
+            // addition
+            Console.WriteLine("{0} + {1} = {2}", a, b, a + b);
+
+            // subtraction
+            Console.WriteLine("{0} - {1} = {2}", a, b, a - b);
+
+            // multiplication
+            Console.WriteLine("{0} * {1} = {2}", a, b, a * b);
+
+            // division
+            Console.WriteLine("{0} / {1} = {2}", a, b, a / b);
+
+            // remainder
+            Console.WriteLine("{0} % {1} = {2}", a, b, a % b);
+        }
+    }
+}
---
+++ Module 3/PE03_Oh/Random.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Random
+{
+    class Random
+    {
+        static void Main(string[] args)
+        {
+            // initializing limits
+            const int MIN = 1;
+            const int MAX = 100;
+
+            // initializing RNG
+            System.Random rnd = new System.Random();
+            int a = rnd.Next(MIN, MAX);
+            int b = rnd.Next(MIN, MAX);
+
+            // addition
+            Console.WriteLine("{0} + {1} = {2}", a, b, a + b);
+
+            // subtraction
+            Console.WriteLine("{0} - {1} = {2}", a, b, a - b);
+
+            // multiplication
+            Console.WriteLine("{0} * {1} = {2}", a, b, a * b);
+
+            // division
+            Console.WriteLine("{0} / {1} = {2}", a, b, a / b);
+
+            // remainder
+            Console.WriteLine("{0} % {1} = {2}", a, b, a % b);
+        }
+    }
+}
