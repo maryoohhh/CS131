cc9986edc3ea4f453246fb521d5beb22fb73e31c
Mary Oh <mary.christine.oh@gmail.com>
2020-04-29 11:50:27 -0700

    Module 4: Lecture PE

---
+++ Module 4/TempConverter/TempConverter/TempConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace TempConverter
+{
+    class Program
+    {
+
+        static double TempConverter(double temperature)
+        {
+            double c = (temperature - 32) * 5 / 9;
+            return c;
+        }
+
+        static void ShowConvertedTemp(double fahrenheit, double celcius)
+        {
+            Console.WriteLine("{0}F = {1}C", fahrenheit, celcius);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the temperature in Fahrenheit: ");
+            double fahrenheit = Double.Parse(Console.ReadLine());
+
+            double celcius = TempConverter(fahrenheit);
+            ShowConvertedTemp(fahrenheit, celcius);
+            Console.Read();
+        }
+    }
+}
