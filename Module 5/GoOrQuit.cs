7918e35d3983049ab914af1deac43854e6e5a3ef
Mary Oh <mary.christine.oh@gmail.com>
2020-05-05 18:43:28 -0700

    PE05: Includes all .cs files

---
+++ Module 5/GoOrQuit.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace GoOrQuit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool runProgram = true;
+
+            // Loop will continue to run until user says no
+            while (runProgram)
+            {
+                Menu();
+                string option = Console.ReadLine();
+
+                // Loop breaks if user says no
+                if (option == "n")
+                {
+                    Console.WriteLine("Okay, bye bye!");
+                    break;
+                }
+
+                // Call StudentGrade method if user says yes
+                else if (option == "y")
+                {
+                    Console.WriteLine("Enter the student's percentage grade: ");
+                    int percentageGrade = Convert.ToInt32(Console.ReadLine());
+
+                    StudentGrade(percentageGrade);
+                }
+
+                // Asks user to re-enter input if response was not y or n
+                else
+                {
+                    Console.WriteLine("Invalid input. Please try again.");
+                }
+            }
+        }
+
+        // Asks user input if they want to re/run the program
+        public static void Menu()
+        {
+            Console.WriteLine("Do you want to run the program (y or n): ");
+        }
+
+        // Initializing method if user decides to run the program
+        static void StudentGrade(int percentageGrade)
+        {
+            // Iterating loop until user inputs an argument that satisfies a condition
+            while (true)
+            {
+                // Loop to check if user input is within range
+                if (0 <= percentageGrade && percentageGrade <= 100)
+                {
+                    // First warning: student is in excellent standing
+                    if (90 <= percentageGrade && percentageGrade <= 100)
+                    {
+                        Console.WriteLine("Student passed the class in excellent standing.");
+                        break;
+                    }
+
+                    // Second warning: student is in good standing
+                    else if (78 <= percentageGrade && percentageGrade < 90)
+                    {
+                        Console.WriteLine("Student passed the class in good standing.");
+                        break;
+                    }
+
+                    // Third warning: student is borderline pass
+                    else if (70 <= percentageGrade && percentageGrade < 78)
+                    {
+                        Console.WriteLine("Student passed the class, but needs to work harder next time.");
+                        break;
+                    }
+
+                    // Default message when student's grade is below passing grade
+                    else
+                    {
+                        Console.WriteLine("Student failed, must retake the class.");
+                        break;
+                    }
+                }
+
+                // Default message if user input is outside of 0-100 percent
+                // Asks user to re-enter a valid input
+                else
+                {
+                    Console.WriteLine("Please enter a valid student's percentage grade: ");
+                    percentageGrade = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+        }
+    }
+}
--- Module 5/WarnLevel.cs
+++ Module 5/WarnLevel.cs
@@ -6,59 +6,50 @@ namespace WarnLevel
     {
         static void Main(string[] args)
         {
-            // Asks for user to input a student's grade
-            Console.WriteLine("Enter the student's percentage grade: ");
-            int percentageGrade = Convert.ToInt32(Console.ReadLine());
+            // Takes user input for desired water temperature
+            Console.WriteLine("Enter the desired water temperature (F):");
+            int temperature = Convert.ToInt32(Console.ReadLine());
 
-            // Calling the method StudentGrade
-            StudentGrade(percentageGrade);
+            // Takes user input if they are sure or not
+            // Assumes all users will only put true or false. Otherwise, code will break
+            Console.WriteLine("Are you sure you want to continue? (true or false):");
+            bool toContinue = Convert.ToBoolean(Console.ReadLine());
+
+            // Calling method WaterTemp
+            WaterTemp(temperature, toContinue);
         }
 
-        // Initializing StudentGrade method that takes one integer input argument
-        static void StudentGrade(int percentageGrade)
+        // Initializing WaterTemp method
+        public static void WaterTemp(int temperature, bool toContinue)
         {
-            // Loop to keep iterating until user inputs an argument that satisfies a condition
-            while (true)
+            // Conditional statement for supercooled water
+            if (temperature == -55 && toContinue == true)
             {
-                // Loop to check if user input is within range
-                if (0 <= percentageGrade && percentageGrade <= 100)
-                {
-                    // First warning: student is in excellent standing
-                    if (90 <= percentageGrade && percentageGrade <= 100)
-                    {
-                        Console.WriteLine("Student passed the class in excellent standing.");
-                        break;
-                    }
+                Console.WriteLine("Warning: Water reached supercooled state!");
+            }
 
-                    // Second warning: student is in good standing
-                    else if (78 <= percentageGrade && percentageGrade < 90)
-                    {
-                        Console.WriteLine("Student passed the class in good standing");
-                        break;
-                    }
+            // Conditional statement for below freezing point
+            else if (temperature < 32 && toContinue == true)
+            {
+                Console.WriteLine("Warning: Temperature is below freezng point of water!");
+            }
 
-                    // Third warning: student is borderline pass
-                    else if (70 <= percentageGrade && percentageGrade < 78)
-                    {
-                        Console.WriteLine("Student passed the class, but needs to work harder next time.");
-                        break;
-                    }
+            // Conditional statement for above boiling point
+            else if (temperature > 212 && toContinue == true)
+            {
+                Console.WriteLine("Warning: Temperature is above boiling point of water!");
+            }
 
-                    // Default message when student's grade is below passing grade
-                    else
-                    {
-                        Console.WriteLine("Student failed, must retake the class.");
-                        break;
-                    }
-                }
+            // Conditional statement if user does not want to continue
+            else if (toContinue == false)
+            {
+                Console.WriteLine("Okay, bye bye!");
+            }
 
-                // Default message if user input is outside of 0-100 percent
-                // Asks user to re-enter a valid input
-                else
-                {
-                    Console.WriteLine("Please enter a valid student's percentage grade: ");
-                    percentageGrade = Convert.ToInt32(Console.ReadLine());
-                }
+            // Default message
+            else
+            {
+                Console.WriteLine("No warning.");
             }
         }
     }
