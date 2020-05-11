4b44a25890d050417b1710a119d9474d508600c7
Mary Oh <mary.christine.oh@gmail.com>
2020-05-11 16:35:48 -0700

    Module 6: PE06 - Created file called NumOut

---
+++ Module 6/NumOut.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace NumOut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Initialize double variable called num
+            double num = 987654.321d;
+
+            // Prints out the original value of num
+            Console.WriteLine("Double: " + num);
+            // Rounds off value of num to two decimal places
+            Console.WriteLine("Truncate to two decimal: " + Math.Round(num, 2));
+            // Converts double num to int
+            Console.WriteLine("Integer: " + Convert.ToInt32(num));
+            // Prints out double num to currency format
+            Console.WriteLine("Currency: " + num.ToString("C"));
+        }
+    }
+}
