9526889ec9db8f272a3dd2ca0078bc7a867a32c0
Mary Oh <mary.christine.oh@gmail.com>
2020-05-11 17:11:48 -0700

    Module 6: PE06 - Created file called MailString.cs

---
+++ Module 6/MailString.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MailString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Takes user input and stores it in FirstName
+            Console.WriteLine("First name: ");
+            string FirstName = Console.ReadLine();
+            // Takes user input and stores it in LastName
+            Console.WriteLine("Last name: ");
+            string LastName = Console.ReadLine();
+
+            // Initializing string called Domain
+            string Domain = "@abc.com";
+
+            // Concatenate strings in an email format
+            string email = string.Concat(char.ToLower(FirstName[0]), LastName.ToLower(), Domain);
+
+            // Prints out string called email
+            Console.WriteLine(email);
+        }
+    }
+}
