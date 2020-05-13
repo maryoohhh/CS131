99775b945a1ea4e637f48bfb7f91187686113312
Mary Oh <mary.christine.oh@gmail.com>
2020-05-13 12:25:47 -0700

    Module 6: PE06 - Updated MailString.cs to only take first 3 char of lastname

--- Module 6/MailString.cs
+++ Module 6/MailString.cs
@@ -7,14 +7,22 @@ namespace MailString
         static void Main(string[] args)
         {
             // Takes user input and stores it in FirstName
-            Console.WriteLine("First name: ");
+            Console.Write("First name: ");
             string FirstName = Console.ReadLine();
             // Takes user input and stores it in LastName
-            Console.WriteLine("Last name: ");
+            Console.Write("Last name: ");
             string LastName = Console.ReadLine();
 
             // Initializing string called Domain
             string Domain = "@abc.com";
+
+            // Checks the length of last name
+            // If LastName is longer than three characters
+            if (LastName.Length >= 3)
+            {
+                // Assigns LastName as the first three characters of the string LastName
+                LastName = LastName.Substring(0, 3);
+            }
 
             // Concatenate strings in an email format
             string email = string.Concat(char.ToLower(FirstName[0]), LastName.ToLower(), Domain);
