181fcf0ccbd2326cf06ed5c827ec2b2ab1f20e1e
Mary Oh <mary.christine.oh@gmail.com>
2020-05-13 12:27:10 -0700

    Module 6: PE06 - Created Expressions.cs file

---
+++ Module 6/Expressions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Expressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            
+            // Asking for user input and storing it
+            Console.Write("Enter a number: ");
+            a = Int32.Parse(Console.ReadLine());
+            Console.Write("Enter another number: ");
+            b = Int32.Parse(Console.ReadLine());
+
+            // addition
+            Console.WriteLine("{0} + {1} = {2}", a, b, a + b);
+
+            // subtraction
+            Console.WriteLine("{0} - {1} = {2}", a, b, a - b);
+
+            // multiplication
+            Console.WriteLine("{0} * {1} = {2}", a, b, a * b);
+
+            // division
+            Console.WriteLine("{0} / {1} = {2}", a, b, a / b);
+
+            // remainder
+            Console.WriteLine("{0} % {1} = {2}", a, b, a % b);
+        }
+    }
+}
