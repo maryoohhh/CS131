ec021d1f1fca5935a852372b42fa3683c0c95e70
Mary Oh <maryoh@marys-mbp.lan>
2020-06-01 22:36:53 -0700

    Module 9: PE09 - .cs files

---
+++ Module 9/Account.cs
@@ -0,0 +1,117 @@
+﻿using System;
+
+namespace Account
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            const decimal OPENING_BALANCE = 55m;
+            const decimal MONTHLY_INTEREST = 0.0033m;
+
+            // Create and use Savings object
+            Savings savings = new Savings(OPENING_BALANCE);
+            savings.AddMonthlyInterest(MONTHLY_INTEREST);
+            savings.ShowBalance();
+
+            // Create and use Checking object
+            Checking checking = new Checking(OPENING_BALANCE);
+            checking.DeductServiceCharge();
+            checking.ShowBalance();
+
+            // Create and use JointSavings object
+            JointSavings jointSavings = new JointSavings("George", "Jane", OPENING_BALANCE);
+            jointSavings.AddMonthlyInterest(MONTHLY_INTEREST);
+            jointSavings.ShowBalance();
+            Console.ReadLine();
+        }
+    }
+
+    // Base class called Account
+    // Parent class
+    class Account
+    {
+        protected decimal BankBalance
+        {
+            set;
+            get;
+        }
+
+        protected string FirstNameA
+        {
+            set;
+            get;
+        }
+
+        protected string FirstNameB
+        {
+            set;
+            get;
+        }
+
+        protected Account()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Inside the Account constructor.");
+        }
+
+        public void ShowBalance()
+        {
+            Console.WriteLine("The balance is " + BankBalance.ToString("C"));
+        }
+    }
+
+    // Derived class - Savings
+    // Child class Savings of parent class Account
+    class Savings : Account
+    {
+        // Default constructor
+        public Savings()
+        {
+            Console.WriteLine("Inside the default Savings constructor.");
+        }
+
+        // Overloading Savings constructor
+        public Savings(decimal balance)
+        {
+            Console.WriteLine("Inside the Savings constructor.");
+            BankBalance = balance;
+        }
+
+        // Custom child method
+        public void AddMonthlyInterest(decimal interest)
+        {
+            BankBalance *= (1.0m + interest);
+        }
+    }
+
+    // Derived class - Checking
+    // Child class Checking of parent class Account
+    class Checking : Account
+    {
+        public Checking(decimal balance)
+        {
+            Console.WriteLine("Inside the Checking constructor.");
+            BankBalance = balance;
+        }
+
+        // Custom child method
+        public void DeductServiceCharge()
+        {
+            const decimal CHECKING_FEE = 1.50m;
+            BankBalance -= CHECKING_FEE;
+        }
+    }
+
+    // Derived class JointSavings inherited from Savings
+    class JointSavings : Savings
+    {
+        public JointSavings(string firstNameA, string firstNameB, decimal balance)
+        {
+            Console.WriteLine("Inside the Joint Savings constructor.");
+            BankBalance = balance;
+            FirstNameA = firstNameA;
+            FirstNameB = firstNameB;
+        }
+    }
+}
---
+++ Module 9/Fashion.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace Fashion
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Fashion fashionDepartment = new Fashion();
+            fashionDepartment.DisplayPromotions();
+            Console.ReadLine();
+        }
+    }
+
+    // Stores sale event data
+    class Promotion
+    {
+        public string Name { get; private set; }
+        public DateTime Start { get; private set; }
+        public DateTime End { get; private set; }
+
+        public Promotion(DateTime start, DateTime end, string name)
+        {
+            Start = start;
+            End = end;
+            Name = name;
+        }
+    }
+
+    // Base class declaration
+    // Abstract class that stores and displays promotional information
+    abstract class Deparment
+    {
+        // Abstract property called DepartmentName
+        public abstract string DepartmentName { get; protected set; }
+
+        protected Promotion[] sales;
+        // Abstract method AssignPromotions
+        protected abstract void AssignPromotions();
+
+
+        // Method DisplayPromotions
+        public virtual void DisplayPromotions()
+        {
+            Console.WriteLine("{0} Deparment Promotions: ", DepartmentName);
+
+            foreach(Promotion sale in sales)
+            {
+                Console.WriteLine("Name: " + sale.Name);
+                Console.WriteLine("Start: " + sale.Start.ToString("m"));
+                Console.WriteLine("End: " + sale.End.ToString("m"));
+            }
+        }
+    }
+
+    // Child class Fashion derived from Department
+    // Implementing class that sets department name and assigns promotions
+    class Fashion : Deparment
+    {
+        // Overriding property DepartmentName
+        public override string DepartmentName { get; protected set; }
+
+        public Fashion()
+        {
+            DepartmentName = "Fashion";
+            AssignPromotions();
+        }
+
+        // Overriding method AssignPromotions
+        protected override void AssignPromotions()
+        {
+            DateTime start = new DateTime(2014, 9, 1);
+            DateTime end = new DateTime(2014, 9, 15);
+            Promotion promotion = new Promotion(start, end, "Fall Sale");
+
+            sales = new Promotion[] { promotion };
+        }
+    }
+}
